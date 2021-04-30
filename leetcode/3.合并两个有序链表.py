b48da81071997349c85bf93a2e5650314a01bc38
LILILIHANG <55121664+LILILIHANG@users.noreply.github.com>
2021-04-30 15:04:45 +0800

    Add files via upload

---
+++ leetcode/3.合并两个有序链表.py
@@ -0,0 +1,20 @@
+# 用户：李航
+# 开发时间：2021/4/30 11:20
+# 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2  # 终止条件，直到两个链表都空
+        if not l2:
+            return l1
+        if l1.val <= l2.val:  # 递归调用
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
