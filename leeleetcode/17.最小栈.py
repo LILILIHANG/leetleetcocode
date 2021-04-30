abb69c42c7abb40d6f83e071a06d7901760505c3
LILILIHANG <55121664+LILILIHANG@users.noreply.github.com>
2021-04-30 15:01:14 +0800

    Add files via upload

---
+++ leeleetcode/17.最小栈.py
@@ -0,0 +1,53 @@
+# 用户：李航
+# 开发时间：2021/4/25 17:11
+
+
+#利用元组存储（当前元素，当前栈内最小值）
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if not self.stack:
+            self.stack.append((x, x))
+        else:
+            self.stack.append((x, min(x, self.stack[-1][1])))
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
+stack=MinStack()
+stack.push(-1)
+stack.push(2)
+stack.push(-2)
+print(stack.getMin())
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
