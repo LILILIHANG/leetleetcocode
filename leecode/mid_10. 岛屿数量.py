3fde5bb6c5f3bcc38e1b6fc5ae6a945321128e6f
LILILIHANG <55121664+LILILIHANG@users.noreply.github.com>
2021-06-02 15:46:14 +0800

    Add files via upload

---
+++ leecode/mid_10. 岛屿数量.py
@@ -0,0 +1,25 @@
+#输入一个由‘0’ 和‘1’ 组成的二维矩阵，0代表水，1代表陆地，
+# 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+#输出岛屿的数量
+class Solution:
+    def dfs(self, grid, r, c):
+        grid[r][c] = 0
+        nr, nc = len(grid), len(grid[0])
+        for x, y in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+            if 0 <= x < nr and 0 <= y < nc and grid[x][y] == "1":
+                self.dfs(grid, x, y)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        nr = len(grid)
+        if nr == 0:
+            return 0
+        nc = len(grid[0])
+
+        num_islands = 0
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == "1":
+                    num_islands += 1
+                    self.dfs(grid, r, c)
+
+        return num_islands
