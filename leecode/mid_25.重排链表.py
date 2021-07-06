eaac03ece3d669788f7d6a7f5bd2fe04f4e79d8f
LILILIHANG <55121664+LILILIHANG@users.noreply.github.com>
2021-07-06 22:18:24 +0800

    Add files via upload

---
+++ leecode/mid_25.重排链表.py
@@ -0,0 +1,68 @@
+
+给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+#1.利用线性表，空间复杂度O(n)
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        if not head:
+            return
+
+        vec = list()
+        node = head
+        while node:
+            vec.append(node)
+            node = node.next
+
+        i, j = 0, len(vec) - 1
+        #前面的节点连接后面一个的节点，然后后面的节点再连接前面的一个节点，实现链表的重排
+        while i < j:
+            vec[i].next = vec[j]
+            i += 1
+            if i == j:
+                break
+            vec[j].next = vec[i]
+            j -= 1
+
+        vec[i].next = None
+
+#2.找中间节点分两部分+反转后一半链表+重新连接
+# 空间复杂度O(1)
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        if not head:
+            return
+
+        mid = self.middleNode(head)
+        l1 = head
+        l2 = mid.next
+        mid.next = None
+        l2 = self.reverseList(l2)
+        self.mergeList(l1, l2)
+
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr:
+            nextTemp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextTemp
+        return prev
+    #和方法1差不多，例：1->2->3  4<-5<-6,也是前面的节点连接后面一个的节点，然后后面的节点再连接前面的一个节点
+    def mergeList(self, l1: ListNode, l2: ListNode):
+        while l1 and l2:
+            l1_tmp = l1.next
+            l2_tmp = l2.next
+
+            l1.next = l2
+            l1 = l1_tmp
+
+            l2.next = l1
+            l2 = l2_tmp
