ea9e3afb95aaa9b06144618e27cbcd436636a1d2
LILILIHANG <55121664+LILILIHANG@users.noreply.github.com>
2021-09-15 09:58:40 +0800

    Update 06.环形链表.py

--- leecode/06.环形链表.py
+++ leecode/06.环形链表.py
@@ -18,3 +18,16 @@ class Solution(object):
             seen.add(head)
             head = head.next
         return False
+
+#快慢指针
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return False
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+        return False
